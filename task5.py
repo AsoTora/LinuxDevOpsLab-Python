a0f2fc9794150e614ff4b5479f4dab42a8c6217e
asotora <andreyw441@gmail.com>
2019-10-29 12:47:53 +0300

    Fixed typos, remove Russian comments and usefull .gitignore file

--- task5.py
+++ task5.py
@@ -1,27 +1,17 @@
 # andrei_shvedau
 # var 5
 
-'''
- Для данной буквы английского алфавита нужно вывести справа стоящую букву на стандартной клавиатуре.
- При этом клавиатура замкнута, т.е. справа от буквы «p» стоит буква «a», от буквы «l» стоит
-буква «z», а от буквы «m» — буква «q».
-'''
-
 
 def keybord():
     keys = "qwertyuiopasdfghjklzxcvbnm"
 
-    c = 0
-    while(True):
+    for c in range(100):  # only 100 tries before collapse
+        print(c)
         k = input()
         if k == 'm':
             print('q')
         else:
             print(keys[keys.find(k) + 1])
-
-        c += 1  # in original task we get a strict input from a file.
-        if c == 100:
-            break
 
 
 if __name__ == '__main__':
